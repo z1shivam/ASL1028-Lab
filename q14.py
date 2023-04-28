d5e8c00d1ddc023c059c08e313dae5fdfeaabccb
dev-shivamk <shivamk.services@gmail.com>
2023-04-29 00:26:06 +0530

    Corrected the code in q14

--- q14.py
+++ q14.py
@@ -1,6 +1,6 @@
-num = float(input("Enter first number: "))
-num = float(input("Enter second number: "))
-num = float(input("Enter third number: "))
+num1 = float(input("Enter first number: "))
+num2= float(input("Enter second number: "))
+num3= float(input("Enter third number: "))
 
 if num1>num2 and num1>num3:
     print("First number is largest")
