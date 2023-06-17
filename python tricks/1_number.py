44a497d441b793f16acbf1e498fca766e269fcda
Shivam <shivamk.services@gmail.com>
2023-06-17 23:09:54 +0530

    new codes

---
+++ python tricks/1_number.py
@@ -0,0 +1,7 @@
+num1 = 1_000_000
+num2 = 5_00
+
+product = num1*num2
+
+print(f"{product:,}")
+#string formatting
---
+++ python tricks/diamond2.py
@@ -0,0 +1,7 @@
+size = int(input("Enter the size of the diamond: "))
+
+for i in range(0,size):
+    print(" "*(size-i) + "*" + " "*(2*i - 1) + "*"*(i != 0))
+
+for i in range(size,-1,-1):
+    print(" "*(size-i) + "*" + " "*(2*i - 1) + "*"*(i != 0))
