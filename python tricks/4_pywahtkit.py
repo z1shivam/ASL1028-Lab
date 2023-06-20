a6df54e3912aa64800fb4b1e9a140043f7d1eb66
Shivam <shivamk.services@gmail.com>
2023-06-20 21:00:48 +0530

    new codes

---
+++ python tricks/4_pywahtkit.py
@@ -0,0 +1,5 @@
+import pywhatkit
+
+# pywhatkit.sendwhatmsg("+919797359032", "Hi", 10, 44)
+
+pywhatkit.search("mayank arya")
---
+++ python tricks/5_datetime.py
@@ -0,0 +1,3 @@
+from datetime import datetime
+c = datetime.now()
+print(f"{c: %d-%m-%y}")
